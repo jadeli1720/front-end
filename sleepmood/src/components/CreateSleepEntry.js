8f2e5fec148c15d425cf83f242df8c800c7f468b
jtmccutcheon <jt.mccutcheon@live.com>
2019-08-28 17:44:32 -0400

    began styling create sleep entry but left commented out whipped together a help page

--- sleepmood/src/components/CreateSleepEntry.js
+++ sleepmood/src/components/CreateSleepEntry.js
@@ -1,10 +1,13 @@
 import React, { useState, useContext } from 'react';
 import { Form } from 'formik';
 //import {Context} from './Context/context'
-
+import styled from 'styled-components'
+import { FeedUser } from 'semantic-ui-react';
 
 export const CreateSleepEntry = () => {
-    const {addSleepEntry} = useState('')
+
+
+    const { addSleepEntry } = useState('')
     const [sleepdate, setSleepdate] = useState('')
     const [wakedate, setWakedate] = useState('')
     const [sleepmood, setSleepmood] = useState('')
@@ -29,54 +32,79 @@ export const CreateSleepEntry = () => {
     };
 
     const submitSleepEntry = e => {
-    e.preventDefault();
-    addSleepEntry(e, sleepdate, wakedate, sleepmood, wakemood, avgmood);
-    setSleepdate('');
-    setWakedate('');
-    setSleepmood('');
-    setWakemood('');
-    setAvgmood('');
+        e.preventDefault();
+        addSleepEntry(e, sleepdate, wakedate, sleepmood, wakemood, avgmood);
+        setSleepdate('');
+        setWakedate('');
+        setSleepmood('');
+        setWakemood('');
+        setAvgmood('');
     };
-    
-    return (
-    <div>
-        <Form onSubmit={submitSleepEntry}>
-            <h2>Add Sleep Entry</h2>
-        <input 
-            onChange={handleSleepdateChange}
-            placeholder = "Sleep Date"
-            value={sleepdate}
-            name="SleepDate"
-            />
-        <input 
-            onChange={handleWakedateChange}
-            placeholder = "Wake Date"
-            value={wakedate}
-            name="Wake Date"
-            />
-            <input 
-            onChange={handleSleepmoodChange}
-            placeholder = "Sleep Mood"
-            value={sleepmood}
-            name="Sleep Mood"
-            />
-            <input 
-            onChange={handleWakemoodChange}
-            placeholder = "Wake Mood"
-            value={wakemood}
-            name="Wake Mood"
-            />
-            <input 
-            onChange={handleAvgmoodChange}
-            placeholder = "Average Mood"
-            value={avgmood}
-            name="Average Mood"
-            />
-            <button>Add Entry</button> 
-        </Form>
-    </div>
+    // const EntryWrap = styled.div` // apply to div later if we end up using this
+    //     margin: 5%;
+    //     h4 {
+    //         color: white;
+    //     }
+    // `;
 
-    ) 
+    // const Form = styled.form`
+    //     background-color: #181827;
+    //     display: flex;
+    //     flex-direction: column;
+    //     justify-content: center;
+    //     text-align: center;
+    //     margin: 5% 0;
+    //     h2 {
+    //         margin: 5% 0;
+    //         color: white;
+    //     }
+    // `;
+
+    return (
+        <div>
+
+            <div className='greeting'>
+                <h4>Good Evening, Charlotte</h4>
+                <h4>It is hh/mm on dd/mm/yyyy.</h4>
+            </div>
+
+            <Form onSubmit={submitSleepEntry} >
+                <h2>Create Sleep Entry</h2>
+                <input
+                    onChange={handleSleepdateChange}
+                    placeholder="Sleep Date"
+                    value={sleepdate}
+                    name="SleepDate"
+                />
+                <input
+                    onChange={handleWakedateChange}
+                    placeholder="Wake Date"
+                    value={wakedate}
+                    name="Wake Date"
+                />
+                <input
+                    onChange={handleSleepmoodChange}
+                    placeholder="Sleep Mood"
+                    value={sleepmood}
+                    name="Sleep Mood"
+                />
+                <input
+                    onChange={handleWakemoodChange}
+                    placeholder="Wake Mood"
+                    value={wakemood}
+                    name="Wake Mood"
+                />
+                <input
+                    onChange={handleAvgmoodChange}
+                    placeholder="Average Mood"
+                    value={avgmood}
+                    name="Average Mood"
+                />
+                <button>Add Entry</button>
+            </Form>
+        </div >
+
+    )
 
 
 }
